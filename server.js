06e1cef5bf318288b2e88614af55a824e53dbe17
GZOZO94 <gaszol11.c@gmail.com>
2017-03-16 21:26:03 +0100

    hello

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var server = http.createServer(function(request,response){
 server.listen(4242);*/
 var express=require('express'),
 	app=express();
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
 var port = process.env.PORT || 3000;
+var data = "{\"key\":\"value\"}";
 	
 app.get("/", function(request,response){
 	response.sendfile("index.html");
@@ -17,3 +20,22 @@ app.get("/", function(request,response){
 var server = app.listen(port, function () {
     console.log('node server is just fine! and running on port - ' + port);
 });
+ 
+client.on('connect', function () {
+  client.subscribe('homerseklet');
+  client.subscribe('nedvesseg');
+})
+ 
+client.on('message', function (topic, message) {
+  console.log(message.toString());
+  console.log(topic);
+})
+var connectionString = "postgres://kldpiqgdkrqypo:be78f021efa0263dd415150760e9d4ea324731c30fde1ff34204c9bb047878cd@ec2-23-21-213-202.compute-1.amazonaws.com:5432:/ddvt14rq2g8n3p"
+var pg = require('pg');
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT * FROM hello', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   })
+ });
